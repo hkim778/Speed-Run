3e54baa506836f2c4504888a3e7a1c57fbb72e14
Hyunjoon <hkim778@uwo.ca>
2019-11-12 11:23:18 -0500

    Character IInitialization

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public GameObject player;
+
+    //offset from the player to camera
+    public Vector3 offset;
+
+    private void Start()
+    {
+        offset = new Vector3(0, 1f, -2f);
+    }
+
+
+    private void LateUpdate()
+    {
+        //TransformPoint uses the offset position from the player Gameobject
+        transform.position = player.transform.TransformPoint(offset);
+
+        //camera will look at the player wherever it is located at
+        transform.LookAt(player.transform);
+    }
+}
