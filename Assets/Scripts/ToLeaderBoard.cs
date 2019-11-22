6e14ab6e5f0494408729f5128971326d421cf4ff
Hyunjoon <hkim778@uwo.ca>
2019-11-22 15:24:05 -0500

    Start screen and Initialization for other scenes

---
+++ Assets/Scripts/ToLeaderBoard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToLeaderBoard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLeaderBoard()
+    {
+        SceneManager.LoadScene("Leader Board");
+    }
+}
